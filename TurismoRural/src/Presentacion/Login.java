e824b8d3306dae74de7f83a75e84343b24636700
davidupup96 <55482517+davidupup96@users.noreply.github.com>
2019-12-13 12:48:40 +0100

    avanzado

--- TurismoRural/src/Presentacion/Login.java
+++ TurismoRural/src/Presentacion/Login.java
@@ -3,10 +3,24 @@ package Presentacion;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JTextField;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.border.TitledBorder;
 
 public class Login {
 
 	private JFrame frame;
+	private JTextField textUsuario;
+	private JTextField textContraseña;
+	private JLabel lblContrasea;
+	private JButton btnAcceder;
+	private JButton btnLimpiar;
 
 	/**
 	 * Launch the application.
@@ -38,6 +52,64 @@ public class Login {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel pnlinicio = new JPanel();
+		pnlinicio.setBorder(new TitledBorder(null, "Login", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		frame.getContentPane().add(pnlinicio, BorderLayout.CENTER);
+		GridBagLayout gbl_pnlinicio = new GridBagLayout();
+		gbl_pnlinicio.columnWidths = new int[]{0, 65, 47, 0, 0, 0, 49, 0, 0, 0};
+		gbl_pnlinicio.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_pnlinicio.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_pnlinicio.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		pnlinicio.setLayout(gbl_pnlinicio);
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		GridBagConstraints gbc_lblUsuario = new GridBagConstraints();
+		gbc_lblUsuario.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUsuario.anchor = GridBagConstraints.EAST;
+		gbc_lblUsuario.gridx = 6;
+		gbc_lblUsuario.gridy = 2;
+		pnlinicio.add(lblUsuario, gbc_lblUsuario);
+		
+		textUsuario = new JTextField();
+		GridBagConstraints gbc_textUsuario = new GridBagConstraints();
+		gbc_textUsuario.insets = new Insets(0, 0, 5, 5);
+		gbc_textUsuario.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textUsuario.gridx = 7;
+		gbc_textUsuario.gridy = 2;
+		pnlinicio.add(textUsuario, gbc_textUsuario);
+		textUsuario.setColumns(10);
+		
+		lblContrasea = new JLabel("Contrase\u00F1a");
+		GridBagConstraints gbc_lblContrasea = new GridBagConstraints();
+		gbc_lblContrasea.insets = new Insets(0, 0, 5, 5);
+		gbc_lblContrasea.anchor = GridBagConstraints.EAST;
+		gbc_lblContrasea.gridx = 6;
+		gbc_lblContrasea.gridy = 3;
+		pnlinicio.add(lblContrasea, gbc_lblContrasea);
+		
+		textContraseña = new JTextField();
+		GridBagConstraints gbc_textContraseña = new GridBagConstraints();
+		gbc_textContraseña.insets = new Insets(0, 0, 5, 5);
+		gbc_textContraseña.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textContraseña.gridx = 7;
+		gbc_textContraseña.gridy = 3;
+		pnlinicio.add(textContraseña, gbc_textContraseña);
+		textContraseña.setColumns(10);
+		
+		btnLimpiar = new JButton("Limpiar");
+		GridBagConstraints gbc_btnLimpiar = new GridBagConstraints();
+		gbc_btnLimpiar.insets = new Insets(0, 0, 5, 5);
+		gbc_btnLimpiar.gridx = 6;
+		gbc_btnLimpiar.gridy = 6;
+		pnlinicio.add(btnLimpiar, gbc_btnLimpiar);
+		
+		btnAcceder = new JButton("Acceder");
+		GridBagConstraints gbc_btnAcceder = new GridBagConstraints();
+		gbc_btnAcceder.insets = new Insets(0, 0, 5, 5);
+		gbc_btnAcceder.gridx = 7;
+		gbc_btnAcceder.gridy = 6;
+		pnlinicio.add(btnAcceder, gbc_btnAcceder);
 	}
 
 }
